fa283178668f7b9043d249c926dee8aa80ff4240
okiselov <kiselov.oleksandr@apriorit.com>
2018-07-01 14:39:41 +0300

    Started to work on book

---
+++ RoyOsherove/FollowBook/OsheroveBook/LogAn.UnitTests/LogAnalyzerTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogAn.UnitTests
+{
+    [TestFixture]
+    public class LogAnalyzerTests
+    {
+        [Test]
+        public void IsValidLogFileName_BadExtension_ReturnsFalse()
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+            bool result = analyzer.IsValidLogFileName("filewithbadextension.foo");
+            Assert.False(result);
+        }
+    }
+}
